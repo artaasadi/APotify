2e6fb4723ca3e3a64db1218acefaf1fc6196a03d
9731006 <9731006@ceit.local>
2019-06-29 14:39:14 +0430

    file Class added (writen by aava)

---
+++ src/GUI/left/File.java
@@ -0,0 +1,41 @@
+package GUI.left;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class File {
+    File myfile;
+    ArrayList<String> path;
+    static int i=0;
+    public File(String filename){
+        myfile=new File(filename+".bin");
+        path=new ArrayList<>();
+
+    }
+    public void addMusic(String path) throws IOException {
+        this.path.add(path);
+        write();
+
+    }
+    public void write() throws IOException {
+        FileOutputStream fileOutputStream=new FileOutputStream(String.valueOf(myfile));
+        ObjectOutputStream o=new ObjectOutputStream(fileOutputStream);
+        o.writeObject(path);
+
+    }
+    public ArrayList<String> readFile() throws IOException {
+        FileInputStream fileInputStream=new FileInputStream(String.valueOf(myfile));
+        ObjectInputStream o=new ObjectInputStream(fileInputStream);
+        ArrayList<String> tmp= null;
+        try {
+            tmp = (ArrayList<String>)o.readObject();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return tmp;
+
+    }
+
+
+}
